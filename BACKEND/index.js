e1ae5a401f40515e7c7a33c5d7e76a42245b8518
adilshaik <shaik.adil@hotmail.com>
2021-11-16 11:17:32 +0530

    Added changed

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,10 +1,14 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Running..");
+app.get('/', (req, res) => {
+  res.send('Running..');
+});
+
+app.get('/api/team', (req, res) => {
+  res.json('');
 });
 
 app.listen(3000, function (err) {
-  console.log("The server is running");
+  console.log('The server is running');
 });
